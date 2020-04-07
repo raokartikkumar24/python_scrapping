03e591b7e5297b23e828138389037fc5c691773e
raokartikkumar <krao24@gmail.com>
2020-04-07 19:50:34 +0530

    initial

---
+++ github_stars/github_stars/spiders/github.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class githubSpider(CrawlSpider):
+	name = 'github_spider'
+
+	start_urls = ['https://github.com/raokartikkumar24?tab=stars']
+
+	rule = (Rule(LinkExtractor(allow='/raokartikkumar24'),),callback='parse_page',)
+
+	def parse_page(self, response):
+		print("parsing URL")
+		print(response.url)
+
+		list_github_starts = response.css('div.d-inline-block > h3 > a::text()').extract()
+		if(len(list_github_starts) < 0):
+			print(list_github_starts)
