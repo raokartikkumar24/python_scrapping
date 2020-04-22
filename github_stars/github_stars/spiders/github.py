409ea5203184221f343170b94d38471131dae70d
raokartikkumar <krao24@gmail.com>
2020-04-22 21:27:11 +0530

    adding csv file

--- github_stars/github_stars/spiders/github.py
+++ github_stars/github_stars/spiders/github.py
@@ -1,4 +1,5 @@
 import scrapy
+import csv
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -13,7 +14,11 @@ class githubSpider(CrawlSpider):
 	def parse_page(self, response):
 		print("parsing URL")
 		print(response.url)
-
+		csv_file = 'github_stars.csv'
 		list_github_starts = response.css('div.d-inline-block > h3 > a::text').extract()
 		if(len(list_github_starts) < 0):
-			print(list_github_starts)
+			for s in list_github_starts:
+				with open(csv_file, 'a+') as f:
+					fwriter = csv.writer(f)
+					csv.writerow(s)
+
