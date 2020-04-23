3b7b37285d09ba5c32878a2fdecfc9849864039d
Kartikkumar <krao24@gmail.com>
2020-04-23 17:51:21 +0530

    Update getjson.py

--- getjson.py
+++ getjson.py
@@ -4,11 +4,11 @@ headers = {
 	'User-Agent' : 'Mozilla'
 }
 
-r = requests.get('https://www.nse-india.com/api/equity-stockIndices?index=NIFTY%2050', headers=headers)
+r = requests.get('https://www.ndtv.com', headers=headers)
 
 returnedJson = r.json()
 
 allStocks = returnedJson['data']
 
 for st in allStocks:
-	print(st['symbol'], st['lastPrice'])
+	print(st['name'])
